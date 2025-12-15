bb0b02e78dc9117d13875f589d7a2db3f77ea4b7
neehanthreddym <neehanthreddym@gmail.com>
2025-12-15 15:45:02 -0600

    Feat: Add Flask web app and prediction pipeline
    Implemented Flask application with UI templates and integrated the model prediction pipeline.

--- src/utils.py
+++ src/utils.py
@@ -82,3 +82,10 @@ def print_models_report(model_report):
             )
     except Exception as e:
         raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
